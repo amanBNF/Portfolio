efeb317bac0986bae41b0cac193e59fcdb98a6c2
Aman Shukla <aman29shukla@gmail.com>
2025-03-13 11:14:19 +0530

    navbar progress

---
+++ PORTFOLIO/src/components/Body.jsx
@@ -0,0 +1,9 @@
+const Body = () => {
+    return(
+        <>
+            <h1>Hello World!</h1>
+        </>
+    )
+}
+
+export default Body;
---
+++ PORTFOLIO/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import './NavStyle.css';
+
+const Navbar = () => {
+    return(
+        <div className="head">
+            <div className="icon">
+                <p>check!</p>
+            </div>
+            <div className="rest">
+                <div className="home">
+                    <h4 href=''>Home</h4>
+                </div>
+                <div className="about">
+                    <h4>About</h4>
+                </div>
+                <div className="contact">
+                    <h4>Contact</h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
