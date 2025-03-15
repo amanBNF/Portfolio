189b0a877fbe73b679d0741b46bcc4d84c332785
Aman Shukla <aman29shukla@gmail.com>
2025-03-15 19:20:11 +0530

    flashcards

--- PORTFOLIO/src/components/Body.jsx
+++ PORTFOLIO/src/components/Body.jsx
@@ -14,6 +14,7 @@ const Body = () => {
                 <p>I'm Aman Shukla, an engineering student specializing in Computer Science with Artificial Intelligence. I'm currently working on Trawell, an AI-powered travel app designed to make trip planning easier and more cost-effective. The app helps users find hidden gems, book local guides, and manage hotels and rentals, all in one place. I'm passionate about AI and how it can enhance real-world experiences, especially in travel. My goal is to create something that makes exploring new places effortless and exciting. Still learning, still building—one step at a time! 🚀</p>
             </div>
         </div>
+        <div className="box">
         <div className="container2">
             <div className="para">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio perspiciatis pariatur eius facere velit. Error, explicabo maxime numquam quod exercitationem voluptatum provident. Quod similique qui fuga distinctio perspiciatis veritatis aliquam.</p>
@@ -22,6 +23,29 @@ const Body = () => {
                 {/* Use the imported image correctly */}
                 <img src={yoda_icon} alt="Yoda Icon" />
             </div>
+        </div>
+        <div class="container3">
+        <div class="card">
+            <div class="card-inner">
+                <div class="card-front">Dataset Analysis</div>
+                <div class="card-back">Working analysis project with pandas and seaborn library</div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-inner">
+                <div class="card-front">Calculator</div>
+                <div class="card-back">Guido van Rossum.</div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-inner">
+                <div class="card-front">What is HTML?</div>
+                <div class="card-back">HTML stands for HyperText Markup Language.</div>
+            </div>
+        </div>
+        </div>
         </div>
         </>
     );
